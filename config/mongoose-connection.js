dd230e2c0d84425c039d0404271716cbe934e66e
Vivek Gawar <vivekgaver2016@gmail.com>
2024-11-08 16:03:23 +0530

    Connection setup of mongoose with different approach and debugger settings

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose')
+const dbgr = require('debug')("development:mongoose")
+const config = require("config")
 
-mongoose.connect("mongodb://127.0.0.1/27017/scatch")
+mongoose
+.connect(`${config.get("MONGODB_URI")}/scatch`) //works on the value of environment variable like this one will take value from the development config file, if we had another file it'll take it from that file
 .then(function(){
-    console.log("connected");
+    dbgr("connected");
 })
 .catch(function(err){
-    console.log(err);
+    dbgr(err);
 })
 
 module.exports = mongoose.connection
